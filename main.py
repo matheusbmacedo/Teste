230d22a42c05d8171da3a9188e213ee00bca38c5
Matheus Macedo <matheus.macedo@artecola.com.br>
2025-05-12 11:47:05 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Olá mundo")
+
+#Variáveis
+
+idade = 34
+nome = "Matheus"
+
+#Print
+
+print(idade)
+
+#Soma
+
+novaIdade = idade + 1
+print(novaIdade)
+
+#Lista
+
+lista = [1,2,3,4,5]
+
+#Métodos
+
+lista_invertida = lista.reverse()
+
+for i in range(10):
+    print(i)
